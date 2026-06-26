2fdaf754ca439206ff16f4afa9c635661320507f
pedro henrick <pedro.h.santos507@aluno.senai.br>
2026-06-26 19:23:14 -0300

    adicionei algumas coisas no meu script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
 const formulario = document.querySelector("#formulario");
 const resultado = document.querySelector("#resultado");
 
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const produto = (document.querySelector("#produto").value);
+    const valor = Number(document.querySelector("#valor").value)
+    const quantidade = Number (document.querySelector("#quantidade").value);
+
+    const listResultado = () => {
+        
+    }
+})
